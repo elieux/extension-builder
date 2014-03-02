c783639b3260a5e15b0a6c2cbc821d6a445baad5
David Macek <david.macek.0@gmail.com>
2014-03-02 23:11:47 +0100

    - fix for invalid pattern in XPI

--- xpi/resources/${product}/lib/main.js.php
+++ xpi/resources/${product}/lib/main.js.php
@@ -1,10 +1,36 @@
 <?php
 
-return'
+$convertPattern = function($pattern) {
+	$containsMoreAsterisks = function($str) {
+		return 1 < preg_match_all('~\*~', $str);
+	};
+	
+	$convertToRegex = function($str) {
+		$quoteRegexPart = function($str) {
+			return preg_quote($str, '/');
+		};
+		
+		$parts = explode("*", $str);
+		$parts = array_map($quoteRegexPart, $parts);
+		return implode(".*", $parts);
+	};
+	
+	if (!$containsMoreAsterisks($pattern))
+		return json_encode($pattern);
+	
+	return "/" . $convertToRegex($pattern) . "/";
+};
+
+$patterns = array();
+foreach ($config['patterns'] as $pattern)
+	$patterns[] = $convertPattern($pattern);
+$patterns = "[" . implode(",", $patterns) . "]";
+
+return '
 var { url } = require("sdk/self").data;
 
 require("sdk/page-mod").PageMod({
-	include: ' . json_encode($config['patterns']) . ',
+	include: ' . $patterns . ',
 	contentScriptFile: [
 		url("include.js"),
 	],
